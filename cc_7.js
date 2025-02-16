ca456e6770967f18ae05289157c6c96045397fb5
Hient <hient@usf.edu>
2025-02-16 13:00:12 -0500

    Adding //testcases and delete console.log

--- cc_7.js
+++ cc_7.js
@@ -4,8 +4,9 @@ function calculateInvoice (subtotal, taxRate, discount){ //Define a function tha
     return `Total Invoice: $${total.toFixed(2)}`; // Format and return
 }
 
-console.log (calculateInvoice(100, 0.08, 5)); //Expected output: "Total Invoice: $103.00"
-console.log (calculateInvoice(500, 0.1, 20)); //Expected output: "Total Invoice: $530.00"
+//Test cases 
+calculateInvoice(100, 0.08, 5); //Expected output: "Total Invoice: $103.00"
+calculateInvoice(500, 0.1, 20); //Expected output: "Total Invoice: $530.00"
 
 //Task 2: Function Expression
 let calculateHourlyWage = function(salary, hoursPerWeek) {
@@ -13,5 +14,24 @@ let calculateHourlyWage = function(salary, hoursPerWeek) {
     return `Hourly Wage: $${hourlyWage.toFixed(2)}`;
 };
 
-console.log (calculateHourlyWage(52000, 40)); // Expected output: "Hourly Wage: $25.00"
-console.log (calculateHourlyWage(75000, 35)); // Expected output: "Hourly Wage: $41.21"
+//Test cases 
+calculateHourlyWage(52000, 40); // Expected output: "Hourly Wage: $25.00"
+calculateHourlyWage(75000, 35); // Expected output: "Hourly Wage: $41.21"
+
+//Task 3: Arrow Function
+let calculateLoyaltyDiscount = (amount, years) => { 
+    let discountRate = 0; //Define a function
+    if (years >=5) discountRate = 0.15; //Discount rate based on years of loyalty
+    else if (years >=3) discountRate = 0.10; // 10% discount for 3 or more years
+    else discountRate = 0.05; //5% discount for less than 3 years
+
+
+    let discountedPrice = amount * (1- discountRate);
+    console.log(`Discounted Price: $${discountedPrice.toFixed(2)}`); //Format and return the discounted price
+};
+
+//Test cases 
+calculateLoyaltyDiscount(100, 6); // Expected output: "Discounted Price: $85.00"
+calculateLoyaltyDiscount(200, 2); // Expected output: "Discounted Price: $190.00"
+
+//Task 4: Parameters and Arguments
